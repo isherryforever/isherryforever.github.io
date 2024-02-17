7349b5de3a66c496bbd59eb3c3582b859c2b18cc
isherryforever <john20190130@gmail.com>
2024-02-17 11:34:12 +0800

    update
    update

--- static/js/aword.js
+++ static/js/aword.js
@@ -10,15 +10,11 @@
     time = 40000;
     arr = [];
     arr[0] = '仰望星空，脚踏实地';
-    arr[1] = '聚散云相似，圆缺月依然';
-    arr[2] = '如竟此后没有炬火，我便是那唯一的光';
-    arr[3] = '海内存知己，天涯若比邻';
-    arr[4] = 'Flee as a bird to your mountain.';
-    arr[5] = '我本将心向明月，奈何明月照沟渠';
-    arr[6] = 'I would rather share one life time with you, than face all the ages of this world alone.';
-    arr[7] = '随风潜入夜，润物细无声';
-    arr[8] = '我见过那样的春天，大地飞花，你在面前';
-    arr[9] = '蓦然回首，那人却在，灯火阑珊处';
+    arr[1] = '海内存知己，天涯若比邻';
+    arr[2] = 'Flee as a bird to your mountain.';
+    arr[3] = 'I would rather share one life time with you, than face all the ages of this world alone.';
+    arr[4] = '随风潜入夜，润物细无声';
+    arr[5] = '我见过那样的春天，大地飞花，你在面前';
     function random() {
         var i, num;
         /*jslint plusplus: true */
